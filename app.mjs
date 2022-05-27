f9ad1588c2e40b37610041b570ba977f3ca7a552
『KūHaku』 <kongarasireeshdevaraj@gmail.com>
2022-05-27 16:00:00 +0530

    not reliable passing new test

--- app.mjs
+++ app.mjs
@@ -24,4 +24,27 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/test', (req, res) => {     
+    const parseIp = (req) =>
+    req.headers['x-forwarded-for']?.split(',').shift()
+    res.send(`<textarea readonly style="font-size: 50px;">${parseIp(req)}</textarea>
+    <h1>• WHOLE PAGE IS A COPY BUTTON</h1>
+    <h2>• ⚡Blazing fastest way to get IP address with less resources</h2>
+    <title>Fetch IP</title>
+    <meta name="definition" content="⚡Blazing fastest way to get IP address with less resources">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+    const copyElement = document.querySelector('textarea');
+    function listener(event) {
+        copyElement.select();
+        copyElement.setSelectionRange(0, copyElement.value.length);
+        document.execCommand('copy');
+    }
+    document.body.addEventListener('click', listener);
+</script>`)});
+
+
+
+
+
 app.listen(port);
