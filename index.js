5ffd3f44ec60ebfb1b135cb90cba92a5a7d91e3d
『KūHaku』 <kongarasireeshdevaraj@gmail.com>
2022-09-01 00:51:42 +0530

    dep changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import fetch from "node-fetch";
+
 import express from "express";
 
 
 const app = express();         
-let port=process.env.PORT || 3000;           
+let port=process.env.PORT || 9000;           
 
 
 
